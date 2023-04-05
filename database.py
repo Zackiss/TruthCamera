7af1d929b566b579759f593022a895c3e60411d7
Hackish <77574790+Zackiss@users.noreply.github.com>
2023-04-05 14:12:15 +0800

    update

--- database.py
+++ database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine, Column, Integer, PickleType
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
 engine = create_engine("sqlite:///chain?check_same_thread=False", echo=True)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
+
 
 # initial implementation - Zackiss on 3.19
 class Chain(Base):
@@ -13,17 +13,16 @@ class Chain(Base):
     id = Column(Integer, primary_key=True)
     attributes = Column(PickleType)
 
+
 def save_block_to_chain(block: dict):
-  session = Session()
-  block = Chain(attributes=block)
-  Chain.__table__.create(engine, checkfirst=True)
-  session.add(block)
-  session.commit()
+    session = Session()
+    block = Chain(attributes=block)
+    Chain.__table__.create(engine, checkfirst=True)
+    session.add(block)
+    session.commit()
+
 
 def get_all_blocks_from_chain() -> list:
-  session = Session()
-  block = [d.content for d in session.query(Chain).all()]
-  return block
-
-
-  
+    session = Session()
+    block = [d.content for d in session.query(Chain).all()]
+    return block
--- server.py
+++ server.py
@@ -1,64 +1,70 @@
 from flask import Flask, request, render_template
-from blockchain import BlockChain 
+from blockchain import BlockChain
 import json
 
 # initial implementation - Zackiss on 3.19
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
-def check_transcation_format(transcation: dict):
-  # verify the transcation in format: {depth_data, pic_hash}
-  if not isinstance(transcation, dict):
-      return False
-  for dam in ["depth_data", "pic_hash"]:
-    if dam not in list(transcation.keys()):
-      return False
-  return True
 
-@app.route('/add_trans',methods = ['POST', 'GET'])
+def check_transaction_format(transaction: dict):
+    # verify the transaction in format: {depth_data, pic_hash}
+    if not isinstance(transaction, dict):
+        return False
+    for dam in ["depth_data", "pic_hash"]:
+        if dam not in list(transaction.keys()):
+            return False
+    return True
+
+
+@app.route('/add_trans', methods=['POST', 'GET'])
 def add_block():
-  transcation = None
-  response = {}
-  
-  if len(request.args):
-    response["status"] = 200
-    response["info"] = "transcation received"
-    transcation = request.args.to_dict().get("transcation", None)
-    
-    # if block not in correct format
-    if transcation is None or not check_transcation_format(transcation):
-      response["status"] = 300
-      response["info"] = "transcation received with incorrect format"
-    # if everything works well, we shall add transcation to blockchain
-    else:
-      blockchain.add_transcation(transcation)
-  else:
-    response["status"] = 500
-    response["info"] = "requesting with empty transcations"
-  return json.dump(response, ensure_ascii=False)
+    transaction = None
+    response = {}
 
-@app.route('/verify_img',methods = ['POST', 'GET'])
-def verify_block():
-  response = {}
-  if len(request.args):
-    response["status"] = 200
-    response["info"] = "pic received"
-    response["result"] = None
-    pic = request.args.to_dict().get("pic_hash", None)
-    if pic is None:
-      response["status"] = 300
-      response["info"] = "transcation received with incorrect format"
-    # if everything works well, we shall verify transcation
+    if len(request.args):
+        response["status"] = 200
+        response["info"] = "transaction received"
+        trans_str = request.args.to_dict().get("transaction", None)
+        transaction = json.loads(trans_str)
+
+        # if block not in correct format
+        if transaction is None or not check_transaction_format(transaction):
+            response["status"] = 300
+            response["info"] = "transaction received with incorrect format"
+        # if everything works well, we shall add transaction to blockchain
+        else:
+            blockchain.add_transaction(transaction)
     else:
-      response["result"] = blockchain.verify_transcation(pic)
-  else:
-    response["status"] = 500
-    response["info"] = "requesting with empty transcations"
-  return json.dump(response, ensure_ascii=False)
-  
+        response["status"] = 500
+        response["info"] = "requesting with empty transactions"
+    return json.dumps(response, ensure_ascii=False)
+
+
+@app.route('/verify_img', methods=['POST', 'GET'])
+def verify_block():
+    response = {}
+    if len(request.args):
+        response["status"] = 200
+        response["info"] = "pic received"
+        response["result"] = False
+        pic = request.args.to_dict().get("pic_hash", None)
+        if pic is None:
+            response["status"] = 300
+            response["info"] = "transaction received with incorrect format"
+        # if everything works well, we shall verify transaction
+        else:
+            response["result"] = blockchain.verify_transaction(pic)
+    else:
+        response["status"] = 500
+        response["info"] = "requesting with empty transaction"
+    return json.dumps(response, ensure_ascii=False)
+
+
 if __name__ == '__main__':
     blockchain = BlockChain()
     app.run()

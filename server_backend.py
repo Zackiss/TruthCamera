eb4758a0139626353e721d3fcb3e58c348278ccf
Matroid_Walker <70513727+TianxingChang@users.noreply.github.com>
2023-04-22 19:21:04 +0800

    blockchain class moved to top

--- server_backend.py
+++ server_backend.py
@@ -13,6 +13,98 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///chain.sqlite"
 db = SQLAlchemy(app)
 cors = CORS(app)
 
+
+# update implementation - Zackiss on 4.7
+# ---------------BLOCKCHAIN part--------------------
+class BlockChain(object):
+    def __init__(self):
+        self.cur_capacity = 0
+        self.max_capacity = 5
+        self.cur_transactions = []
+        self.chain = [{
+            "index": 0,
+            "timestamp": time.time(),
+            "transactions": self.cur_transactions,
+            "proof": 12100,
+            "previous_hash": 12100
+        }]
+        self.first_hash = self.hash({
+            "index": len(self.chain) + 1,
+            "timestamp": time.time(),
+            "transactions": self.cur_transactions,
+            "proof": 12100,
+            "previous_hash": 12100
+        })
+
+    def get_chain_fin(self):
+        return self.chain[-1]
+
+    def get_chain(self) -> list[dict]:
+        self.chain = get_all_blocks_from_chain()
+        return self.chain
+
+    def new_block(self, proof, previous_hash=None):
+        # create a new block and save it to the chain
+        block = {
+            "index": len(self.chain) + 1,
+            "timestamp": time.time(),
+            "transactions": self.cur_transactions,
+            "proof": proof,
+            "previous_hash": previous_hash if previous_hash else self.first_hash
+        }
+        save_block_to_chain(block)
+        self.chain = self.get_chain()
+        self.cur_capacity = 0
+        print("adding finished")
+
+    def add_transaction(self, transaction):
+        assert self.cur_capacity <= self.max_capacity
+        # if block out of capacity
+        if self.cur_capacity == self.max_capacity:
+            print("adding to blockchain")
+            prev_block = self.get_chain_fin()
+            proof = self.proof_work(prev_block["proof"])
+            self.new_block(proof, previous_hash=self.hash(prev_block))
+            
+        # if block not full, add transaction to the block
+        else:
+            self.cur_transactions.append(transaction)
+            self.cur_capacity += 1
+            print("buffering: " + str(self.cur_transactions))
+            print("current capacity reach to: " + str(self.cur_capacity))
+            print("max capacity: " + str(self.max_capacity))
+
+    def verify_transaction(self, pic) -> bool:
+        self.get_chain()
+        print("obtaining: " + str(pic))
+        print("verifying with chain: " + str(self.chain))
+        for block in self.chain:
+            for transaction in block["transactions"]:
+                if pic == '"' + transaction["pic_hash"] + '"':
+                    print("hash found!")
+                    return True
+        print("hash not found!")
+        return False
+
+    def hash(self, block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    def proof_work(self, prev_proof):
+        proof = random.randint(0, 150)
+        while not self.verify_proof(prev_proof, proof):
+            proof += random.randint(0, 150)
+        return proof
+
+    @staticmethod
+    def verify_proof(last_proof, proof) -> bool:
+        guess = f"{last_proof}{proof}".encode()
+        hashy = hashlib.sha256(guess).hexdigest()
+        return hashy[:3] == "042"
+   
+
+blockchain = BlockChain()
+    
 # --------------TIPS---------------------
 # you shall post or get your request at addresses: ip/add_trans, ip/verify_img
 # you shall upload json with format {"transaction": {"depth_data": depth data, "pic_hash": pic hash}}
@@ -108,97 +200,7 @@ def verify_block():
     return json.dumps(response, ensure_ascii=False)
 
 
-# update implementation - Zackiss on 4.7
-# ---------------BLOCKCHAIN part--------------------
-class BlockChain(object):
-    def __init__(self):
-        self.cur_capacity = 0
-        self.max_capacity = 5
-        self.cur_transactions = []
-        self.chain = [{
-            "index": 0,
-            "timestamp": time.time(),
-            "transactions": self.cur_transactions,
-            "proof": 12100,
-            "previous_hash": 12100
-        }]
-        self.first_hash = self.hash({
-            "index": len(self.chain) + 1,
-            "timestamp": time.time(),
-            "transactions": self.cur_transactions,
-            "proof": 12100,
-            "previous_hash": 12100
-        })
-
-    def get_chain_fin(self):
-        return self.chain[-1]
-
-    def get_chain(self) -> list[dict]:
-        self.chain = get_all_blocks_from_chain()
-        return self.chain
-
-    def new_block(self, proof, previous_hash=None):
-        # create a new block and save it to the chain
-        block = {
-            "index": len(self.chain) + 1,
-            "timestamp": time.time(),
-            "transactions": self.cur_transactions,
-            "proof": proof,
-            "previous_hash": previous_hash if previous_hash else self.first_hash
-        }
-        save_block_to_chain(block)
-        self.chain = self.get_chain()
-        self.cur_capacity = 0
-        print("adding finished")
-
-    def add_transaction(self, transaction):
-        assert self.cur_capacity <= self.max_capacity
-        # if block out of capacity
-        if self.cur_capacity == self.max_capacity:
-            print("adding to blockchain")
-            prev_block = self.get_chain_fin()
-            proof = self.proof_work(prev_block["proof"])
-            self.new_block(proof, previous_hash=self.hash(prev_block))
-            
-        # if block not full, add transaction to the block
-        else:
-            self.cur_transactions.append(transaction)
-            self.cur_capacity += 1
-            print("buffering: " + str(self.cur_transactions))
-            print("current capacity reach to: " + str(self.cur_capacity))
-            print("max capacity: " + str(self.max_capacity))
-
-    def verify_transaction(self, pic) -> bool:
-        self.get_chain()
-        print("obtaining: " + str(pic))
-        print("verifying with chain: " + str(self.chain))
-        for block in self.chain:
-            for transaction in block["transactions"]:
-                if pic == '"' + transaction["pic_hash"] + '"':
-                    print("hash found!")
-                    return True
-        print("hash not found!")
-        return False
-
-    def hash(self, block):
-        block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
-
-    def proof_work(self, prev_proof):
-        proof = random.randint(0, 150)
-        while not self.verify_proof(prev_proof, proof):
-            proof += random.randint(0, 150)
-        return proof
-
-    @staticmethod
-    def verify_proof(last_proof, proof) -> bool:
-        guess = f"{last_proof}{proof}".encode()
-        hashy = hashlib.sha256(guess).hexdigest()
-        return hashy[:3] == "042"
-
-
 if __name__ == '__main__':
-    blockchain = BlockChain()
     app.run()
 
 

066e92d4646106cf2530356b6ddf705971aa7a8f
Hackish <77574790+Zackiss@users.noreply.github.com>
2023-04-26 19:26:03 +0800

    Update server_backend.py

--- server_backend.py
+++ server_backend.py
@@ -60,27 +60,29 @@ class BlockChain(object):
 
     def add_transaction(self, transaction):
         if testing:
+            self.cur_transactions.append(transaction)
             prev_block = self.get_chain_fin()
             proof = self.proof_work(prev_block["proof"])
             self.new_block(proof, previous_hash=self.hash(prev_block))
             print("test buffering: " + str(self.cur_transactions))
             print("running with minimum block capacity")
             return
-        assert self.cur_capacity <= self.max_capacity
-        # if block out of capacity
-        if self.cur_capacity == self.max_capacity:
-            print("adding to blockchain")
-            prev_block = self.get_chain_fin()
-            proof = self.proof_work(prev_block["proof"])
-            self.new_block(proof, previous_hash=self.hash(prev_block))
-            
-        # if block not full, add transaction to the block
         else:
-            self.cur_transactions.append(transaction)
-            self.cur_capacity += 1
-            print("buffering: " + str(self.cur_transactions))
-            print("current capacity reach to: " + str(self.cur_capacity))
-            print("max capacity: " + str(self.max_capacity))
+            assert self.cur_capacity <= self.max_capacity
+            # if block out of capacity
+            if self.cur_capacity == self.max_capacity:
+                print("adding to blockchain")
+                prev_block = self.get_chain_fin()
+                proof = self.proof_work(prev_block["proof"])
+                self.new_block(proof, previous_hash=self.hash(prev_block))
+
+            # if block not full, add transaction to the block
+            else:
+                self.cur_transactions.append(transaction)
+                self.cur_capacity += 1
+                print("buffering: " + str(self.cur_transactions))
+                print("current capacity reach to: " + str(self.cur_capacity))
+                print("max capacity: " + str(self.max_capacity))
 
     def verify_transaction(self, pic) -> bool:
         self.get_chain()
